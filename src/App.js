a69bd5ccbc6b1bd652902180ac3efa932ee362d7
yunieom <yunzoo0915@gmail.com>
2020-11-30 10:58:47 +0900

    Nov 30, 2020

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component  {
+  render () {
+    return (
+    <div className="App">
+      Hello, React!!
+    </div>
+    );
+  }
+    
+}
+
+export default App;
