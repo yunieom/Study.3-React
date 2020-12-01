d43ea969f9708beec3d9e85f1152763986348a36
yunieom <yunzoo0915@gmail.com>
2020-12-01 20:42:43 +0900

    Dec 1, 2020
    React 13/40

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import './App.css';
 import { Component } from 'react';
 
+class Subject extends Component {
+  render () {
+    return (
+      <header>
+          <h1>{this.props.title}</h1>
+          {this.props.sub}
+      </header>
+    );
+  }
+}
+
+class TOC extends Component {
+  render () {
+    return (
+      <nav>
+          <ul>
+              <li><a href="1.html">HTML</a></li>
+              <li><a href="2.html">CSS</a></li>
+              <li><a href="3.html">JavaScript</a></li>
+          </ul>            
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render () {
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    );
+  }
+}
+
 class App extends Component  {
   render () {
     return (
     <div className="App">
-      Hello, React!!
+      <Subject title="WEB" sub="world wide web!"></Subject>
+      <Subject title="React" sub="For UI"></Subject>
+      <TOC></TOC>
+      <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
     </div>
     );
   }
